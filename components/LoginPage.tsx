7dc4be1b33a9df596ad1d0b974bf758fcb729165
longk2tha-rgb <longk2tha@gmail.com>
2025-10-30 16:53:48 +0700

    Create LoginPage.tsx

---
+++ components/LoginPage.tsx
@@ -0,0 +1,11 @@
+// components/LoginPage.tsx
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Login Page</h1>
+      <p>This is a placeholder login page. Replace with your own UI.</p>
+    </div>
+  );
+}
